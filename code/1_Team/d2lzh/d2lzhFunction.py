7a8eed166d6c9709284001a9b98f4829718de2e3
blacksevenzqj <397353721@qq.com>
2020-02-14 20:28:22 +0800

    First

---
+++ code/1_Team/d2lzh/d2lzhFunction.py
@@ -0,0 +1,104 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import time
+from IPython import display
+from matplotlib import pyplot as plt
+
+
+class Timer(object):
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        # start the timer
+        self.start_time = time.time()
+
+    def stop(self):
+        # stop the timer and record time into a list
+        self.times.append(time.time() - self.start_time)
+        return self.times[-1]
+
+    def avg(self):
+        # calculate the average and return
+        return sum(self.times)/len(self.times)
+
+    def sum(self):
+        # return the sum of recorded time
+        return sum(self.times)
+
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg') 
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+
+# 梯度计算
+def sgd(params, lr, batch_size):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    for param in params:
+        param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
+
+
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, optimizer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            y_hat = net(X)
+            l = loss(y_hat, y).sum()
+            
+            # 梯度清零
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+            
+            l.backward()
+            if optimizer is None:
+                d2l.sgd(params, lr, batch_size)
+            else:
+                optimizer.step() 
+            
+            
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+
+def load_data_jay_lyrics(path):
+    with open(path, encoding='utf-8') as f:
+        corpus_chars = f.read()
+    corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+    corpus_chars = corpus_chars[0:10000]
+    idx_to_char = list(set(corpus_chars))
+    char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+
+
+# if __name__ == '__main__':
+#     test()
